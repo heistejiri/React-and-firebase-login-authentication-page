9ee69e331e19ce096bf3ec488d11e923dc0c2dad
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-09 17:54:30 +0100

    update index.js

--- src/login.js
+++ src/login.js
@@ -1,9 +1,42 @@
 import React from "react";
 
-const login = () => {
+const login = (props) => {
+
+  const { 
+    email, 
+    setEmail, 
+    setPassword, 
+    handleLogin, 
+    handleSignUp, 
+    hasAccount, 
+    setHasAccount, 
+    emailError, 
+    passwordError,
+  } = props;
+
    return(
       <section className="login">
-          
+         <div className="loginContainer">
+          <label>UserName</label>
+          <input
+            type="text"
+            autoFocus
+            required
+            value={email}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="errorMsg">{emailError}</p>
+          <label>Password</label>
+          <input
+            type="password"
+            autoFocus
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+         </div>
       </section>
    )
 }
+
+export default login;
