2eaccc58c25ab91e1f328708b5af2f2195a78ced
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-10 08:16:19 +0100

    Hero.js
    Home page after login in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{ useState, useEffect } from "react";
 import fire from "./fire"; 
 import Login from "./Login";
+import Hero from "./hero";
 import './App.css';
 
 const App = () => {
---
+++ src/hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+
+  return(
+      <section className="Hero">
+        <nav>
+          <h2>Welcome to Tejiri's Page</h2>
+          <button>Logout</button>
+        </nav>
+      </section>
+  );
+};
+
+export default Hero;
