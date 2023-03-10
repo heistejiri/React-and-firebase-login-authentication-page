8d7804a8b157db51581582e4dfa6e6077d134346
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-03-10 08:41:42 +0100

    Update hero.js
    finished the home page design

--- src/hero.js
+++ src/hero.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = (handleLogout) => {
 
   return(
-      <section className="Hero">
+      <section className="hero">
         <nav>
           <h2>Welcome to Tejiri's Page</h2>
           <button onClick={handleLogout}>Logout</button>
